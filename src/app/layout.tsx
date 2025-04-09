8136f827e1e41f135c364663d9aa58584b9d416a
Jiekang Hu <54252549+huj13k4n9@users.noreply.github.com>
2025-04-09 20:13:43 +0800

    Add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,12 @@ import Header from "@/components/header";
 import Background from "@/components/background";
 
 export const metadata: Metadata = {
+    generator: 'Next.js',
+    applicationName: config.site.title,
     title: config.site.title,
     description: config.site.description,
+    referrer: "origin-when-cross-origin",
+    authors: [ {name: config.site.author} ],
 };
 
 export default function RootLayout({children}: Readonly<{
@@ -16,13 +20,10 @@ export default function RootLayout({children}: Readonly<{
 }>) {
     return (
         <html lang="zh">
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        </head>
         <body className={`min-w-xs overflow-x-hidden dark`}>
         <Background>
             <Header/>
-            <main className="max-w-4xl mx-auto px-8 py-6">
+            <main className="max-w-4xl mx-auto px-6 py-6">
                 {children}
             </main>
             <Footer/>
