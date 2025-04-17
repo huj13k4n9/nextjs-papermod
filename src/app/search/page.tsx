a364c62f69144514c8ece0233a89b4faf0f99046
Jiekang Hu <54252549+huj13k4n9@users.noreply.github.com>
2025-04-18 01:27:00 +0800

    Animate SearchResult a little...

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,6 +2,7 @@
 
 import React, {useCallback, useEffect, useState} from "react";
 import {LuSearch} from "react-icons/lu";
+// @ts-ignore
 import {searchArticles, SearchResult} from "@/lib/search";
 import Link from "next/link";
 import {AnimatePresence, motion} from "motion/react";
@@ -79,6 +80,39 @@ function SearchIndicator({count}: { count: number }) {
     )
 }
 
+function SearchResult({results}: { results: SearchResult[] }) {
+    return (
+        <AnimatePresence mode="wait" initial={false}>
+            {results.length !== 0 &&
+                <>
+                    <motion.div
+                        className="mt-6 mb-8 columns-2 gap-4"
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        exit={{opacity: 0}}
+                    >
+                        {results.map((result, index) => (
+                            <Link
+                                key={`result-${index}`}
+                                href={result.slug}
+                                className="space-y-3 block mb-4 p-4 border-2 rounded-lg break-inside-avoid"
+                            >
+                                <h2 className={`text-2xl font-semibold leading-snug`}>{highlightedText(result.title)}</h2>
+                                <div className={`text-sm leading-normal`}>
+                                    {result.content.map((item, contentIndex) => (
+                                        <p key={`content-${contentIndex}`}>...{highlightedText(item)}...</p>
+                                    ))}
+                                </div>
+                            </Link>
+                        ))}
+                    </motion.div>
+                    <p className={`text-center mb-8`}>已加载所有结果</p>
+                </>
+            }
+        </AnimatePresence>
+    )
+}
+
 export default function Search() {
     const [query, setQuery] = useState('');
     const [results, setResults] = useState<SearchResult[]>([]);
@@ -113,23 +147,7 @@ export default function Search() {
                         placeholder="Search Anything..."
                         className="w-full px-3 py-2 border-2 rounded-lg"
                     />
-
-                    <div className="mt-6 columns-2 gap-4">
-                        {results.map((result, index) => (
-                            <Link
-                                key={index}
-                                href={result.slug}
-                                className="space-y-2.5 block mb-4 p-4 border-2 rounded-lg break-inside-avoid"
-                            >
-                                <h3 className={`text-2xl font-semibold`}>{highlightedText(result.title)}</h3>
-                                <div className={`text-sm leading-normal`}>
-                                    {result.content.map((item, index) => (
-                                        <p key={index}>...{highlightedText(item)}...</p>
-                                    ))}
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
+                    <SearchResult results={results} />
                 </div>
             </div>
         </div>
